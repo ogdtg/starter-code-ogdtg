81e640e1150c2bc8565562d0cf21e23e95631ea2
OGD Kanton Thurgau <ogd@tg.ch>
2023-06-22 23:43:54 +0200

    Create update.R

---
+++ update.R
@@ -0,0 +1,54 @@
+print(getwd())
+library(stringr)
+#library(tgAPI)
+library(dplyr)
+library(httr)
+library(jsonlite)
+
+catalog <- httr::GET("https://data.tg.ch/api/explore/v2.0/catalog/exports/json") 
+catalog <- catalog$content %>% rawToChar() %>& jsonlite::fromJSON() 
+
+catalog_mod <- catalog %>%
+  filter(str_detect(dataset_id,"^[a-z]+-[a-z]+-\\d+$"))
+
+
+
+fileName <- "pattern/pattern.ipynb"
+prep_data <- readChar(fileName, file.info(fileName)$size)
+id <- catalog_mod$dataset_id[5]
+res <- httr::GET(glue::glue("https://data.tg.ch/api/explore/v2.0/catalog/datasets/{id}"))
+
+metadata <- res$content %>%
+  rawToChar() %>%
+  jsonlite::fromJSON()
+
+
+fields <- metadata$dataset$fields$name
+
+if (length(fields)>0) {
+  license <- metadata$dataset$metas$default$license
+  title <- metadata$dataset$metas$default$title
+  modified <- metadata$dataset$metas$default$modified
+  keywords <- paste(metadata$dataset$metas$default$keyword, collapse= ", ")
+  description <- metadata$dataset$metas$default$description %>% str_replace_all('\\"',"\\'")
+  fields <- metadata$dataset$fields
+  created <- metadata$dataset$metas$dcat$issued
+
+  prep_data_mod <- stringr::str_replace_all(prep_data,c("_DATANAME_"=title,
+                                                        "_CREATED_"=created,
+                                                        "_MODIFIED_"= modified,
+                                                        "_LICENSE_"=license,
+                                                        "_DESCRIPTION_"=description,
+                                                        "_KEYWORDS_"=keywords,
+                                                        "_IDENTIFIER_"=id,
+                                                        "pattern/pattern" = paste0("ogdtg@",id))
+                                                )
+  prep_data_mod <- stringr::str_remove_all(prep_data_mod,"\\r")
+  con <- file(paste0("ogdtg@",id,".ipynb"), open = "wt", encoding = "UTF-8")
+
+
+  sink(con)
+  cat(prep_data_mod)
+  sink()
+  close(con)
+}
