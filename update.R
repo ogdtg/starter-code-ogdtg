2920254a31a53d05f14b537a89fa04849f939342
OGD Kanton Thurgau <ogd@tg.ch>
2023-06-22 23:55:59 +0200

    Update update.R

--- update.R
+++ update.R
@@ -13,7 +13,9 @@ catalog_mod <- catalog %>%
 
 
 
-fileName <- "/__w/starter-code-ogdtg/starter-code-ogdtg/pattern/pattern.ipynb"
+#fileName <- "/__w/starter-code-ogdtg/starter-code-ogdtg/pattern/pattern.ipynb"
+fileName <- "~/pattern/pattern.ipynb"
+
 prep_data <- readChar(fileName, file.info(fileName)$size)
 id <- catalog_mod$dataset_id[5]
 res <- httr::GET(glue::glue("https://data.tg.ch/api/explore/v2.0/catalog/datasets/{id}"))
@@ -44,7 +46,7 @@ if (length(fields)>0) {
                                                         "pattern/pattern" = paste0("ogdtg@",id))
                                                 )
   prep_data_mod <- stringr::str_remove_all(prep_data_mod,"\\r")
-  con <- file(paste0("/__w/starter-code-ogdtg/starter-code-ogdtg/ogdtg@",id,".ipynb"), open = "wt", encoding = "UTF-8")
+  con <- file(paste0("~/ogdtg@",id,".ipynb"), open = "wt", encoding = "UTF-8")
 
 
   sink(con)
